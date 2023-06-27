6d56ff2b63d1f07234ecef82360df1a42c2fb011
GDJ59 <GDJ59@DESKTOP-9HO3F2D>
2023-06-27 14:22:12 +0900

    yjc-dto 생성

---
+++ src/main/java/dto/Reservation.java
@@ -0,0 +1,23 @@
+package dto;
+
+import java.sql.Time;
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Reservation {
+	private int rv_num;
+	private String user_id;
+	private int lane_num;
+	private Date rv_now;
+	private Date rv_date;
+	private int rv_game;
+	private Time rv_start;
+	private Time rv_end;
+	private int rv_people;
+}
