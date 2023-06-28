a7a7aaeb83cb520040c0ded2eb3e55c5cafcf1be
82109 <82109@DESKTOP-O4NN41P>
2023-06-28 14:17:12 +0900

    6월28일 승준이형꺼 커밋

--- src/main/java/dto/Board.java
+++ src/main/java/dto/Board.java
@@ -2,6 +2,8 @@ package dto;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotEmpty;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,7 +14,9 @@ import lombok.ToString;
 public class Board {
 	private int board_num;
 	private String user_id;
+	@NotEmpty(message="제목을 입력하세요")
 	private String board_title;
+	@NotEmpty(message="내용을 입력하세요")
 	private String board_content;
 	private int board_readcnt;
 	private String board_id;
