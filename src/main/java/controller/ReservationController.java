50d0f7f90dcdcb70bdf90ff69a108c90aaead596
JongChane <yjc3533@gmail.com>
2023-06-30 15:17:48 +0900

    yjc-sitemesh 수정

--- src/main/java/controller/ReservationController.java
+++ src/main/java/controller/ReservationController.java
@@ -2,8 +2,11 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import dto.Reservation;
 import dto.ReservationService;
 
 @Controller
@@ -11,4 +14,12 @@ import dto.ReservationService;
 public class ReservationController {
 	@Autowired
 	private ReservationService rvService;
+
+	@GetMapping("*")
+	public ModelAndView rv() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject(new Reservation());
+		return mav;
+	}
+
 }
