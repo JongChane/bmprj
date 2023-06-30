ef221b95426e1e99df9f6b7f7d1cb79bd39bac92
JongChane <yjc3533@gmail.com>
2023-06-30 15:14:44 +0900

    yjc

--- src/main/java/controller/ReservationController.java
+++ src/main/java/controller/ReservationController.java
@@ -1,10 +1,14 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import dto.ReservationService;
 
 @Controller
 @RequestMapping("reservation")
 public class ReservationController {
-
+	@Autowired
+	private ReservationService rvService;
 }
